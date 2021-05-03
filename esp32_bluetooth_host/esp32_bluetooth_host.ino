9ef081105990b0ed9bcf56767e07b390d6c83204
Jonathan Matarazzi <git@jonathanm.nl>
2021-05-03 13:22:15 +0200

    Added indicator LED

--- esp32_bluetooth_host/esp32_bluetooth_host.ino
+++ esp32_bluetooth_host/esp32_bluetooth_host.ino
@@ -13,8 +13,12 @@
 #include "esp_gap_bt_api.h"
 #include "driver\rtc_io.h"
 
-#define PAIRING_TIME 30000
+#define PAIRING_TIME 10000
+#define PAIR_BLINK_INTERVAL 250
 
+#define BLUE_PIN 12
+#define RED_PIN 13
+#define MPU_POWER_PIN 19
 #define PAIR_PIN 26
 #define POWER_OFF_PIN 32
 #define POWER_OFF_PIN_GPIO GPIO_NUM_32
@@ -26,37 +30,48 @@ BluetoothSerial SerialBT;
 const int MPU_addr = 0x68; // I2C address of the MPU-6050
 byte output[12];
 
-TaskHandle_t readIMU, bluetoothHandler, pairingTask;
+bool transmitMode;
+
+TaskHandle_t readIMU, clientHandler, pairingTask;
 
 
 void setup() {
   Serial.begin(115200);
 
   //Set pinmodes for slider en button
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(MPU_POWER_PIN, OUTPUT);
   pinMode(PAIR_PIN, INPUT_PULLUP);
   pinMode(POWER_OFF_PIN, INPUT_PULLUP);
   pinMode(WIRED_MODE_PIN, INPUT_PULLUP);
   pinMode(WIRELESS_MODE_PIN, INPUT_PULLUP);
+
+  digitalWrite(BLUE_PIN, HIGH);
+  digitalWrite(RED_PIN, HIGH);
+  digitalWrite(MPU_POWER_PIN, HIGH);
+
+  delay(100);
 
   //Wake up MPU-6050 and set accelerometer and gyroscope to correct ranges
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(false);
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x1B);
   Wire.write(0b00011000);
   Wire.endTransmission(false);
   Wire.beginTransmission(MPU_addr);
+  Wire.write(0x1C);
   Wire.write(0b00011000);
   Wire.endTransmission(false);
-  Wire.endTransmission(true);
 
   //Enable bluetooth
   startBluetoothStack();
-  SerialBT.begin("WirelessIMU-"+getBluetoothAddress());
+  SerialBT.begin("WirelessIMU-" + getBluetoothAddress());
   discoverable(false);
-  Serial.println("Not discoverable");
 
   //Create repeating tasks
   xTaskCreatePinnedToCore(
@@ -70,12 +85,12 @@ void setup() {
   );
 
   xTaskCreatePinnedToCore(
-    bluetoothHandlerCode,
-    "BluetoothHandler",
+    clientHandlerCode,
+    "ClientHandler",
     10000,
     NULL,
     1,
-    &bluetoothHandler,
+    &clientHandler,
     1
   );
 
@@ -106,24 +121,40 @@ void readIMUCode( void * parameter) {
   }
 }
 
-void bluetoothHandlerCode ( void * parameters) {
+void clientHandlerCode ( void * parameters) {
   for (;;) {
-
-    if (SerialBT.available() > 0) {
-      SerialBT.read();
-      SerialBT.write(output, 12);
+    if (transmitMode) {
+      if (SerialBT.available() > 0) {
+        SerialBT.read();
+        SerialBT.write(output, 12);
+      }
+    } else {
+      if (Serial.available() > 0) {
+        Serial.read();
+        Serial.read();
+        Serial.write(output, 12);
+      }
     }
   }
 }
 
 void pairingCode ( void * parameters) {
   for (;;) {
-    if (digitalRead(PAIR_PIN) == LOW) {
-      discoverable(true);
-      Serial.println("Discoverable");
-      delay(PAIRING_TIME);
-      discoverable(false);
-      Serial.println("Not discoverable");
+    if (transmitMode == true) {
+      if (digitalRead(PAIR_PIN) == LOW) {
+        discoverable(true);
+        for (int i = 0; i < (PAIRING_TIME / PAIR_BLINK_INTERVAL); i++) {
+          digitalWrite(BLUE_PIN, LOW);
+          delay(PAIR_BLINK_INTERVAL / 2);
+          digitalWrite(BLUE_PIN, HIGH);
+          delay(PAIR_BLINK_INTERVAL / 2);
+        }
+        discoverable(false);
+      } else {
+        digitalWrite(BLUE_PIN, LOW);
+      }
+    } else {
+      digitalWrite(BLUE_PIN, HIGH);
     }
   }
 }
@@ -145,27 +176,15 @@ String getBluetoothAddress() {
   return addrString;
 }
 
-void printWakeupReason() {
-  esp_sleep_wakeup_cause_t wakeupReason;
-  wakeupReason = esp_sleep_get_wakeup_cause();
-
-  switch (wakeupReason) {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeupReason); break;
-  }
-}
-
 void startSleep() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);
   Wire.write(0b01001000);
   Wire.endTransmission(false);
+  digitalWrite(MPU_POWER_PIN, LOW);
+  digitalWrite(BLUE_PIN, HIGH);
+  digitalWrite(RED_PIN, HIGH);
   esp_sleep_enable_ext0_wakeup(POWER_OFF_PIN_GPIO, 1);
-  Serial.println("start sleep");
   esp_deep_sleep_start();
 }
 
@@ -181,5 +200,12 @@ void discoverable(bool dis) {
 void loop() {
   if (digitalRead(POWER_OFF_PIN) == LOW) {
     startSleep();
+  } else if (digitalRead(WIRED_MODE_PIN) == LOW) {
+    transmitMode = false;
+    digitalWrite(BLUE_PIN, HIGH);
+    digitalWrite(RED_PIN, LOW);
+  } else if (digitalRead(WIRELESS_MODE_PIN) == LOW) {
+    transmitMode = true;
+    digitalWrite(RED_PIN, HIGH);
   }
 }
