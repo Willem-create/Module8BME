50ac65678bd84c1c687635e3e2452544958bb1ea
Willem van Dijk <w.h.p.vandijk@student.utwente.nl>
2021-05-10 16:27:06 +0200

    Changed the test file to actually test

--- python_bluetooth_client/test.py
+++ python_bluetooth_client/test.py
@@ -1,5 +1,17 @@
-from ImuSensor import ImuSensor, MODE_WIRED, MODE_WIRELESS
+import csv
 
-sensor = ImuSensor(MODE_WIRELESS, 'WirelessIMU-0FD6')
+csv_index1 = 0
+csv_index2 = 0
+start = False
+with open('sensorA.csv', mode='w') as sensorA:
+    fieldnames = ['index', 'accX', 'accY', 'accZ', 'gyroX', 'gyroY', 'gyroZ']
+    writerA = csv.DictWriter(sensorA, fieldnames=fieldnames)
+    writerA.writeheader()
+    start = True
 
-print(sensor.take_measurement())
+
+
+while start:
+    writerA.writerow({'index': '1', 'accX': '1', 'accY': '1', 'accZ': '1', 'gyroX': '1',
+                      'gyroY': '1', 'gyroZ': '1'})
+    csv_index1 += 1
